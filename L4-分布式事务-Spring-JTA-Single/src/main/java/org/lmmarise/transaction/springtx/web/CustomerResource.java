6208439dca6b7f2e6a41fa4f368debd15de3dc2d
lmmarisej <lmmarise@foxmail.com>
2022-09-26 01:35:44 +0800

    Add Spring JTA 事务初体验

---
+++ L4-分布式事务-Spring-JTA-Single/src/main/java/org/lmmarise/transaction/springtx/web/CustomerResource.java
@@ -0,0 +1,44 @@
+package org.lmmarise.transaction.springtx.web;
+
+import org.lmmarise.transaction.springtx.dao.CustomerRepository;
+import org.lmmarise.transaction.springtx.domain.Customer;
+import org.lmmarise.transaction.springtx.service.CustomerServiceTxInAnnotation;
+import org.lmmarise.transaction.springtx.service.CustomerServiceTxInCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/customer")
+public class CustomerResource {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerResource.class);
+
+    @Autowired
+    private CustomerServiceTxInAnnotation customerService;
+    @Autowired
+    private CustomerServiceTxInCode customerServiceInCode;
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @PostMapping("/annotation")
+    public Customer createInAnnotation(@RequestBody Customer customer) {
+        LOG.info("CustomerResource create in annotation create customer:{}", customer.getUsername());
+        return customerService.create(customer);
+    }
+
+    @PostMapping("/code")
+    public Customer createInCode(@RequestBody Customer customer) {
+        LOG.info("CustomerResource create in code create customer:{}", customer.getUsername());
+        return customerServiceInCode.create(customer);
+    }
+
+    @GetMapping("")
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+
+}
